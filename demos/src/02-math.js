3fe1380dac5d3f1ab15312a0a8b0a40a3a01e230
Hugomndez <hello@hugomendez.dev>
2022-05-18 21:47:40 -0500

    Add math test

---
+++ demos/src/02-math.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return null;
+  return a / b;
+}
+module.exports = {
+  sum,
+  multiply,
+  divide,
+};
