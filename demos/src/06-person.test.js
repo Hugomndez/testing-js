4ce48a785bb21b77fce38dc8be64fb5808e6cb77
Hugomndez <hello@hugomendez.dev>
2022-05-22 19:42:36 -0500

    Unitary test

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -3,13 +3,19 @@ const Person = require('./06-person');
 describe('Test for person', () => {
   let person;
 
+  // Arrange
   beforeEach(() => {
     person = new Person('Nicolas', 45, 1.7);
   });
 
   test('should return down', () => {
+    // Arrange
     person.weight = 45;
+
+    // Act
     const imc = person.calcIMC();
+
+    // Assert
     expect(imc).toBe('down');
   });
 
