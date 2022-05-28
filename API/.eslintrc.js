07508811b94dedeeefd602e1e368827a37560ac0
Hugomndez <hello@hugomendez.dev>
2022-05-28 16:02:43 -0500

    Add faker js

--- API/.eslintrc.js
+++ API/.eslintrc.js
@@ -6,12 +6,11 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
 };
--- API/src/services/books.service.test.js
+++ API/src/services/books.service.test.js
@@ -1,19 +1,25 @@
+const { generateManyBooks } = require('../fakes/book.fake');
 const BooksService = require('./books.service');
 
-const fakeBooks = [
-  {
-    _id: 1,
-    name: 'Harry Potter',
-  },
-];
+// const fakeBooks = [
+//   {
+//     _id: 1,
+//     name: 'Harry Potter',
+//   },
+// ];
+
+const mockGetAll = jest.fn();
 
 const MongoLibStub = {
-  getAll: () => [...fakeBooks],
+  getAll: mockGetAll,
   create: () => {},
 };
 
 jest.mock('../lib/mongo.lib', () =>
-  jest.fn().mockImplementation(() => MongoLibStub)
+  jest.fn().mockImplementation(() => ({
+    getAll: mockGetAll,
+    create: () => {},
+  }))
 );
 
 describe('Test for BooksService', () => {
@@ -27,22 +33,28 @@ describe('Test for BooksService', () => {
   describe('Test for getBooks', () => {
     test('Should return a book list', async () => {
       // Arrange
-
+      const fakeBooks = generateManyBooks(20);
+      mockGetAll.mockResolvedValue(fakeBooks);
       // Act
       const books = await service.getBooks({});
       console.log(books);
       // Assert
-      expect(books.length).toEqual(1);
+      expect(books.length).toEqual(fakeBooks.length);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
     });
 
     test('Should return a book list', async () => {
       // Arrange
+      const fakeBooks = generateManyBooks(4);
 
+      mockGetAll.mockResolvedValue(fakeBooks);
       // Act
       const books = await service.getBooks({});
       console.log(books);
       // Assert
-      expect(books[0].name).toEqual('Harry Potter');
+      expect(books[0].name).toEqual(fakeBooks[0].name);
     });
   });
 });
